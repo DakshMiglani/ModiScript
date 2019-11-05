ba6d7169d7f549f7ce2a83f8f42f0987d36e8a86
Daksh Miglani <hello@dak.sh>
2019-11-05 19:17:52 +0530

    perfect stdout works

--- web/main.py
+++ web/main.py
@@ -1,13 +1,30 @@
+import sys
+sys.path.append("..")
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from modiscript.api import ModiScript
 from modiscript.utils import ErrorHandler
-
+from io import StringIO
 app = Flask(__name__)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 ms = ModiScript()
+
+def captureOutput(func):
+    def inner(*args, **kwargs):
+        sys.stdout = StringIO()
+
+        func(*args, **kwargs)
+
+        return sys.stdout.getvalue()
+    return inner
+
+@captureOutput
+def execute(code):
+    ms.execute(code, "code")
+
 
 @app.route("/", methods=["POST"])
 def run():
@@ -18,6 +35,6 @@ def run():
         return jsonify({ "error": "Executable code has not been provided." }), 400
 
     try:
-        return jsonify({ "out": ms.execute(code, "code") }), 200
+        return jsonify({ "out": execute(code) }), 200
     except ErrorHandler as e:
         return jsonify({ "error": str(e) }), 402
